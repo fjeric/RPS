481ef28409896f5b2c0d4a51bdfbfe82ecc7e104
fjeric <ericlee6196@gmail.com>
2022-05-05 17:18:31 -0400

    update with buttons, limitless counter

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-const compChoice = computerPlay();
+var playerScore = 0;
+var compScore = 0;
+var roundWinner = '';
 
 function computerPlay() {
     var x = Math.floor(Math.random() * 3);
@@ -20,59 +22,80 @@ function playerSelection() {
 }
 
 function playRound(playerChoice, compChoice) {
-    console.log(playerChoice);
-    if (playerChoice == 'rock' && compChoice == 'scissor')
-       return 'You win! Rock beats Scissor';
-    if (playerChoice == 'paper' && compChoice == 'rock')
-        return 'You win! Paper beats Rock';
-    if (playerChoice == 'scissor' && compChoice == 'paper')
-        return 'You win! Scissor beats Paper';
+    if (
+    (playerChoice == 'rock' && compChoice == 'scissor') ||
+    (playerChoice == 'paper' && compChoice == 'rock') ||
+    (playerChoice == 'scissor' && compChoice == 'paper') 
+    ) {
+        playerScore++;
+        roundWinner = 'player';
+    }
 
-    if (compChoice == 'rock' && playerChoice == 'scissor')
-       return 'You lose! Rock beats Scissor';
-    if (compChoice == 'paper' && playerChoice == 'rock')
-        return 'You lose! Paper beats Rock';
-    if (compChoice == 'scissor' && playerChoice == 'paper')
-        return 'You lose! Scissor beats Paper';
+    if (
+    (compChoice == 'rock' && playerChoice == 'scissor') ||
+    (compChoice == 'paper' && playerChoice == 'rock') ||
+    (compChoice == 'scissor' && playerChoice == 'paper')
+    ) {
+        compScore++;
+        roundWinner = 'computer';
+    }
 
     if (compChoice == playerChoice) 
-       return "It's a tie! You both picked " + playerChoice;
+       roundWinner = 'tie';
 }
 
-function game() {
-    var playerWins = 0;
-    var compWins = 0;
-    var ties = 0;
-    for (let i = 0; i < 5; i++) {
-        var playerChoice = playerSelection();
-        var compChoice = computerPlay();
-        var roundWinner = playRound(playerChoice, compChoice);
-
-        if (roundWinner.includes('win')) {
-            playerWins = playerWins + 1;
-        }
-        if (roundWinner.includes('lose')) {
-            compWins = compWins + 1;
-        }
-        if (roundWinner.includes('tie')) {
-            ties = ties + 1;
-        }
-
-        console.log(roundWinner);
-    }
-    console.log('Player wins: ' +playerWins);
-    console.log('Computer Wins: ' +compWins);
-    console.log('Ties: ' +ties);
-
-    if (playerWins == compWins) {
-        console.log("Player and Comp have the same number of wins. It's a tie!");
-    }
-    if (playerWins > compWins) {
-        console.log('Congratulations, you win!')
-    }
-    else {
-        console.log('Computer wins :(');
-    }
+function playerClick(playerChoice) {
+    const compChoice = computerPlay();
+    playRound(playerChoice, compChoice);
+    roundWin();
+    choices(playerChoice, compChoice);
+    updateScore();
 }
 
 
+const buttons = document.querySelectorAll('button');
+const container = document.querySelector('#container');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerClick(button.id);
+    });
+});
+
+const playerScoreCount = document.getElementById('playerScore');
+const compScoreCount = document.getElementById('compScore');
+const scoreMessage = document.getElementById('message');
+const choiceMessage = document.getElementById('choiceInfo')
+
+function roundWin() {
+    if (roundWinner === 'tie') {
+        scoreMessage.textContent = "It's a tie!";
+    }
+    if (roundWinner === 'player') {
+        scoreMessage.textContent = "You won the round!";
+    }
+    if (roundWinner === 'computer') {
+        scoreMessage.textContent = "The computer won the round";
+    }
+}
+
+function choices(playerChoice, compChoice) {
+    if (roundWinner === 'tie') {
+        choiceMessage.textContent = `You both picked ${capitalizeChoice(playerChoice)}`
+    }
+    if (roundWinner === 'player') {
+        choiceMessage.textContent = `${capitalizeChoice(playerChoice)} beats ${capitalizeChoice(compChoice)}`
+    }
+    if (roundWinner === 'computer') {
+        choiceMessage.textContent = `${capitalizeChoice(playerChoice)} loses to ${capitalizeChoice(compChoice)}`
+    }
+}
+
+function updateScore() {
+    playerScoreCount.textContent = `Player: ${playerScore}`
+    compScoreCount.textContent = `Computer: ${compScore}`
+}
+
+function capitalizeChoice(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
